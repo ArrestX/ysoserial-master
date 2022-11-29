6716fd4bb6b2f2b28584e5c6d53e2d2c98bc5d8f
韩世龙 <slhanf@isoftstone.com>
2022-11-29 14:57:47 +0800

    学习javassist里边的CtClass

--- src/main/java/ysoserial/payloads/javaAssistTest.java
+++ src/main/java/ysoserial/payloads/javaAssistTest.java
@@ -1,17 +1,20 @@
 package ysoserial.payloads;
 
-import javassist.ClassClassPath;
-import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.NotFoundException;
+import javassist.*;
 import ysoserial.payloads.util.Gadgets;
 
 public class javaAssistTest {
-    public static void main(String[] args) throws NotFoundException {
+    public static void main(String[] args) throws NotFoundException, CannotCompileException {
         ClassPool classPool = ClassPool.getDefault();
         //获取系统的类，已经加载完的
-        CtClass cc  =classPool.get("java.lang.String");
-        cc.debugWriteFile("D:\\");
+        CtClass ctClass = classPool.makeClass("test.test");
+        CtField ctField = new CtField(classPool.get("java.lang.String"),"name",ctClass);
+        ctField.setModifiers(Modifier.PUBLIC| Modifier.STATIC);
+        ctClass.addField(ctField,"name=\"aaa\";");
+        ctClass.debugWriteFile("D:\\");
+
+//        CtClass cc  =classPool.get("java.lang.String");
+//        cc.debugWriteFile("D:\\");
         //找对应的javaclsspath
 //        classPool.appendClassPath(new ClassClassPath(Gadgets.StubTransletPayload.class));
         //自己定义一个类
