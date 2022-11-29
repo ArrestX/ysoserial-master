37593f8b60c22994ecbeefc6e01dace80701119b
arrest <hansl666@126.com>
2022-11-29 22:37:31 +0800

    代码审计javaassist的学习

--- src/main/java/ysoserial/payloads/javaAssistTest.java
+++ src/main/java/ysoserial/payloads/javaAssistTest.java
@@ -20,27 +20,41 @@ public class javaAssistTest {
         ctClass.getField("name");
         //javaassist构造方法
         CtConstructor ctConstructor = new CtConstructor(new CtClass[]{},ctClass);
-        ctConstructor.setBody("{System.out.print(2);}");
+        ctConstructor.setBody("{System.out.print(999);}");
         ctClass.addConstructor(ctConstructor);
 
-        //todo 这里有bug 修改不成功
+        //todo 这里有bug 修改不成功，注意类对地址的引用
 
 
-        CtClass c2 = classPool.get("ysoserial.aaa");
-        CtConstructor ctConstructor2 = c2.getConstructors()[0];
-        ctConstructor2.insertBefore("System.out.print(567);");
+//        CtClass c2 = classPool.get("ysoserial.payloads.aaa");
+//        CtConstructor ctConstructor2 = c2.getConstructors()[0];
+//        c2.makeClassInitializer().setBody("{System.out.print(33333);}");
+//        ctConstructor2.insertBefore("System.out.print(567);");
+//        ctConstructor2.insertAfter("System.out.print(789);");
 //        ctClass.debugWriteFile("D:\\");
 //接着写入某个class里
-        new FileOutputStream("2.class").write(c2.toBytecode());
-        ctClass.toBytecode();
+//        new FileOutputStream("2.class").write(c2.toBytecode());
+//        ctClass.toBytecode();
         //然后再一步步反射，defineclassloader去调用
-        ctClass.toClass().newInstance();
+//        ctClass.toClass().newInstance();
 //        CtClass cc  =classPool.get("java.lang.String");
 //        cc.debugWriteFile("D:\\");
         //找对应的javaclsspath
 //        classPool.appendClassPath(new ClassClassPath(Gadgets.StubTransletPayload.class));
         //自己定义一个类
 //        CtClass cc2 = classPool.makeClass("com.until.text");
+        CtConstructor ct2 = new CtConstructor(new CtClass[]{classPool.get("java.lang.String"),classPool.get("java.lang.String")},ctClass);
+        ct2.setBody("{$0.name=$1;}");
+        ctClass.addConstructor(ct2);
+        new FileOutputStream("2.class").write(ctClass.toBytecode());
 
+
+    }
+}
+class aaa{
+    public static String name = "aaa";
+    public aaa(String b,int c) {
+        this.name = b;
+        System.out.print(222);
     }
 }
