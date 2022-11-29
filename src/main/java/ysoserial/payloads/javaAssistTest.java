00bce157b9bdf74c0639effbd7564fa90705a0a9
韩世龙 <slhanf@isoftstone.com>
2022-11-29 17:08:32 +0800

    学习javassist里边的CtClass

--- src/main/java/ysoserial/payloads/javaAssistTest.java
+++ src/main/java/ysoserial/payloads/javaAssistTest.java
@@ -3,16 +3,31 @@ package ysoserial.payloads;
 import javassist.*;
 import ysoserial.payloads.util.Gadgets;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class javaAssistTest {
-    public static void main(String[] args) throws NotFoundException, CannotCompileException {
+    public static void main(String[] args) throws NotFoundException, CannotCompileException, IOException, InstantiationException, IllegalAccessException {
         ClassPool classPool = ClassPool.getDefault();
         //获取系统的类，已经加载完的
         CtClass ctClass = classPool.makeClass("test.test");
-        CtField ctField = new CtField(classPool.get("java.lang.String"),"name",ctClass);
-        ctField.setModifiers(Modifier.PUBLIC| Modifier.STATIC);
-        ctClass.addField(ctField,"name=\"aaa\";");
-        ctClass.debugWriteFile("D:\\");
-
+        //获取父类
+        ctClass.setInterfaces(new CtClass[]{classPool.get("java.io.Serializable")});
+        ctClass.setSuperclass(classPool.get("ysoserial.GeneratePayload"));
+        CtField ctField = new CtField(classPool.get("java.lang.String"), "name", ctClass);
+        ctField.setModifiers(Modifier.PUBLIC | Modifier.STATIC);
+        ctClass.addField(ctField, "name=\"aaa\";");
+        ctClass.getField("name");
+        //javaassist构造方法
+        CtConstructor ctConstructor = new CtConstructor(new CtClass[]{},ctClass);
+        ctConstructor.setBody("{System.out.print(2);}");
+        ctClass.addConstructor(ctConstructor);
+//        ctClass.debugWriteFile("D:\\");
+//接着写入某个class里
+        new FileOutputStream("3.class").write(ctClass.toBytecode());
+        ctClass.toBytecode();
+        //然后再一步步反射，defineclassloader去调用
+        ctClass.toClass().newInstance();
 //        CtClass cc  =classPool.get("java.lang.String");
 //        cc.debugWriteFile("D:\\");
         //找对应的javaclsspath
